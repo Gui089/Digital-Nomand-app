74bbe499f53b9d756a7630f048b1bbb0dd0a197c
Guilherme Coutinho Silva <guilhermesilba811@gmail.com>
2025-07-13 22:37:22 -0300

    feat: add animation close bottomSheet

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { StyleSheet, TouchableOpacity } from "react-native";
-import Animated, { SharedValue, useAnimatedStyle, useDerivedValue, useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, { SharedValue, useAnimatedStyle, useDerivedValue, useSharedValue, withDelay, withTiming } from "react-native-reanimated";
 
 type BottomSheetProps = {
   isOpen: SharedValue<boolean>;
@@ -9,15 +9,16 @@ type BottomSheetProps = {
 };
 
 export function BottomSheet({ children, onPress, isOpen, duration = 600 }: PropsWithChildren<BottomSheetProps>) {
+  const progress = useDerivedValue(() => withTiming(isOpen.value ? 0 : 1, { duration }));
 
   const backDropAnimatedStyle =
     useAnimatedStyle(() => ({
-      zIndex: isOpen.value ? 1 : -1
+      opacity: 1 - progress.value,
+      zIndex: isOpen.value ? 1 : withDelay(duration, withTiming(-1, {duration: 0}))
     }));
 
   const height = useSharedValue(0);
 
-  const progress = useDerivedValue(() => withTiming(isOpen.value ? 0 : 1, { duration }));
 
   const sheetAnimatedStyle = useAnimatedStyle(() => ({
     transform: [
